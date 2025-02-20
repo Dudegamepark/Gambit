85ef46d70f95bf1b89fa382986b36b3f88e5c878
shawn <digitalp@cs.washington.edu>
2025-02-19 23:14:49 -0800

    ratman bug

--- ratman.js
+++ ratman.js
@@ -34,7 +34,7 @@ class Dialog {
         if (this.line < this.lines.length) {
             document.getElementById('ratman1lines').innerHTML = this.getLine();
         } else {
-            location.href='mainmenu.html';
+            location.href='mainMenu.html';
             console.log("Dialog end.");
         }
     }
